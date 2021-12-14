40113f1d68e1d344be6b3e090626c34022b76660
KoppePan0709 <yuichi.masutani@ibm.com>
2021-12-14 21:52:28 +0900

    set params in .env

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
 const express　=　require('express');
 const fs = require('fs');
-const axios = require('axios'); 
+const axios = require('axios');
+require('dotenv').config();
+console.log(process.env.CHANNEL_ID_PROD)
 
-// const CHANNEL_ID = "C0285T7SL4X"; // kmp_kk_出社状況確認
-const CHANNEL_ID = "C02QMRLRQ75"; // baymax-sandbox
+// Slack Configureation
+// const CHANNEL_ID = process.env.CHANNEL_ID_PROD; // kmp_kk_出社状況確認
+const CHANNEL_ID = process.env.CHANNEL_ID_TEST; // baymax-sandbox
+const API_KEY = process.env.API_KEY
 const API_ENDPOINT = "https://slack.com/api"
 
+// Date
 const date  = new Date();
 const year  = date.getFullYear();
 const month = date.getMonth() + 1;
 const day   = date.getDate();
 const today = (year + '年' + month + '月' + day + '日');
 
+
+// Express 
 const app = express();
-const port=process.env.PORT || 8080;
-app.get('/', (req, res)=>{ res.send('Hello, World'); });
-app.listen(port, ()=>{ console.log(`Express Server Listen START at port=${port}`); });
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Express Server Listen START at port=${port}`);
+});
+
+app.get('/', (request, response) => {
+     response.send('Hello, World'); 
+});
 
 
-const message = JSON.parse(fs.readFileSync('./message_template.json', 'utf8'));
-message.channel = CHANNEL_ID
-message.blocks[0].text.text = "*" + today + "の出社状況*<!channel>"
-
+// Edit messages
+const messages = JSON.parse(fs.readFileSync('./message_template.json', 'utf8'));
+messages.channel = CHANNEL_ID
+messages.blocks[0].text.text = "*" + today + "の出社状況*<!channel>"
 
 
 async function postAttendanceCheckPoll(){ 
     const headers = {
         "content-type": "application/json",
-        "Authorization": 'Bearer xoxb-2264321203926-2673794707732-sNrpL1mQn3Wn1vgVHQWkmFL3'
+        "Authorization": 'Bearer ' + API_KEY
     }
     try { 
-        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", message, { headers: headers })
+        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
         console.log(response.data); 
     } catch (error) { 
         console.log(error.response.body); 
     } 
 }
 
-// const headers = {
-//             "content-type": "application/json",
-//             "Authorization": 'Bearer xoxb-2264321203926-2673794707732-sNrpL1mQn3Wn1vgVHQWkmFL3'
-//         }
-
-// axios.post(API_ENDPOINT + "/chat.postMessage", message, {headers: headers}).then(response => {
-//     console.log(response.data);
-// })
-
 postAttendanceCheckPoll()
+
+
+
 
