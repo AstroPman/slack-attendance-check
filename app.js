67c0fec07d7e6d7744c882e2b775e68537474e4a
KoppePan0709 <yuichi.masutani@ibm.com>
2021-12-15 00:23:13 +0900

    added function to add and delete user from realtime DB

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const express　=　require('express');
 const fs = require('fs');
 const axios = require('axios');
-const admin = require("firebase-admin");
+require('dotenv').config();
 
-// Fetch the service account key JSON file contents
-const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY_PATH);
-
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://slack-attendance-check-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
-var db = admin.database();
 
 // Date
 const date  = new Date();
@@ -21,22 +12,32 @@ const day   = date.getDate();
 const today = (year + '年' + month + '月' + day + '日');
 const todayString = year.toString() + month.toString() + day.toString()
 
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-
-var attendance = db.ref("/attendance/" + todayString);
-attendance.set({
-  osaki: "test1,test2"
+// Firebase Realtime Database
+const admin = require("firebase-admin");
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY_PATH);
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://slack-attendance-check-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
+const db = admin.database();
+const osaki = db.ref("/attendance/" + todayString + "/osaki");
+const home = db.ref("/attendance/" + todayString + "/home");
+const others = db.ref("/attendance/" + todayString + "/others");
 
-require('dotenv').config();
-console.log(process.env.CHANNEL_ID_PROD)
+osaki.on('value', snapshot => {
+    console.log(snapshot.val())
+})
+// attendance.set({
+//   osaki: "test1,test2"
+// });
+
 
 // Slack Configureation
 // const CHANNEL_ID = process.env.CHANNEL_ID_PROD; // kmp_kk_出社状況確認
 const CHANNEL_ID = process.env.CHANNEL_ID_TEST; // baymax-sandbox
 const API_KEY = process.env.API_KEY
 const API_ENDPOINT = "https://slack.com/api"
-
 
 // Express 
 const app = express();
@@ -47,8 +48,6 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Express Server Listen START at port=${port}`);
 });
-
-
 
 
 // Edit messages
@@ -76,6 +75,25 @@ app.get('/', (request, response) => {
 });
 
 app.post('/endpoint', (request, response) => {
-    console.log(request.body.payload)
+    const value = request.body.payload.actions[0].value
+    const userName = "<@" + request.body.payload.user.name + ">"
+    if (value == "osaki") {
+        osaki.on('value', snapshot => {
+            let attendants = snapshot.val().split(',')
+            // 追加処理
+            if (!attendants.includes(userName)) {
+                attendants.push(userName)
+                const newAttendants = attendants.join(',')
+            }
+            //削除処理
+            else {
+                const filteredAttendants =  attendants.filter( user => {
+                    return !user == userName
+                })
+                const newAttendants = filteredAttendants.join(',')
+            }
+        osaki.set(newAttendants)
+        })
+    }
     response.send(''); 
 });
