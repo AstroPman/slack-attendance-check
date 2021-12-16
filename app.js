1dca6c29950c585aa819a5a30a482f67081864a4
KoppePan0709 <yuichi.masutani@ibm.com>
2021-12-16 22:15:54 +0900

    separeted files based on  functionality

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express　=　require('express');
-const fs = require('fs');
-const axios = require('axios');
 require('dotenv').config();
-
-
-const firebaseDb =  require('./firebase.js');
-const weatherData = require('./weather.js')
 const cron = require('node-cron');
 
-
-
-// Slack Configureation
-const CHANNEL_ID = process.env.CHANNEL_ID_PROD; // kmp_kk_出社状況確認
-// const CHANNEL_ID = process.env.CHANNEL_ID_TEST; // baymax-sandbox
-const API_KEY = process.env.API_KEY
-const API_ENDPOINT = "https://slack.com/api"
+const firebaseDb =  require('./src/firebase.js');
+const functions = require('./src/functions.js');
 
 
 // Express 
@@ -28,130 +17,6 @@ app.listen(port, () => {
 });
 
 
-
-
-
-// Functions
-function getToday () {
-    // Date
-    const date  = new Date();
-    const year  = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day   = date.getDate();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const dayOfWeek = date.getDay()
-    const dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek]
-    const today = year + '年' + month + '月' + day + '日' + '(' + dayOfWeekStr + ')';
-    const todayString = year.toString() + month.toString() + day.toString()
-    return [today, todayString, dayOfWeekStr, day]
-}
-
-async function postAttendanceCheckPoll(){
-    // Weather
-    const weather = await weatherData()
-
-    // Edit messages
-    const messages = JSON.parse(fs.readFileSync('./message_template.json', 'utf8'));
-    messages.channel = CHANNEL_ID
-    messages.attachments[0].blocks[0].text.text = getToday()[0] + "の出社状況"
-    messages.attachments[0].blocks[2].text.text =`*出社状況を教えてください。*<!channel>\n今日の大崎の天気: ${weather.description}\n:small_orange_diamond: 最高気温: *${weather.maxTemp}℃*\n:small_blue_diamond: 最低気温: *${weather.minTemp}℃*`
-    messages.attachments[0].blocks[2].accessory.image_url = weather.iconUrl
-    // Headers
-    const headers = {
-        "content-type": "application/json",
-        "Authorization": 'Bearer ' + API_KEY
-    }
-
-    
-    // API CALL
-    try { 
-        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
-        console.log(response.data); 
-    } catch (error) { 
-        console.log(error.response.body); 
-    } 
-}
-
-async function updateAttendanceCheckPoll(timestamp, attendants){
-    messages.ts = timestamp
-    for (item in attendants) {
-        const text = attendants[item].join(',')
-        console.log(attendants[item].length)
-        const cnt = attendants[item].length
-        if (item == "home") {
-            messages.attachments[1].blocks[0].text.text = "*在宅*\n" + text
-            messages.attachments[1].blocks[1].elements[0].text = "合計" + cnt + "人"
-        }
-        else if (item == "osaki") {
-            messages.attachments[2].blocks[0].text.text = "*大崎*\n" + text
-            messages.attachments[2].blocks[1].elements[0].text = "合計" + cnt + "人"
-        }
-        else {
-            messages.attachments[3].blocks[0].text.text = "*その他*\n" + text
-            messages.attachments[3].blocks[1].elements[0].text = "合計" + cnt + "人"
-        }
-    }
-    // Headers
-    const headers = {
-        "content-type": "application/json",
-        "Authorization": 'Bearer ' + API_KEY
-    }
-    console.log(messages)
-    // API CALL
-    try { 
-        const response = await axios.post(API_ENDPOINT + "/chat.update", messages, { headers: headers })
-        console.log(response.data); 
-    } catch (error) { 
-        console.log(error.response.body); 
-    } 
-}
-
-async function postAttendanceCheckRemind(){
-    // Edit messages
-    const messages = JSON.parse(fs.readFileSync('./message_template.json', 'utf8'));
-    messages.channel = CHANNEL_ID
-    messages.attachments[0].blocks[0].text.text = getToday()[0] + "の出社状況（リマインド）"
-    messages.attachments[0].blocks[2].text.text = ":bangbang:*未回答の方はご回答お願いいたします。*:bangbang:"
-    delete messages.attachments[0].blocks[2].accessory
-    messages.attachments.splice(1,messages.attachments.length - 1)
-    messages.attachments[0].blocks.splice(3, 1)
-
-    const headers = {
-        "content-type": "application/json",
-        "Authorization": 'Bearer ' + API_KEY
-    }
-
-    // API CALL
-    try { 
-        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
-        console.log(response.data); 
-    } catch (error) { 
-        console.log(error.response.body); 
-    } 
-}
-
-async function postCloudMeeting(){
-    // Edit messages
-    const messages = JSON.parse(fs.readFileSync('./message_template_cloud_meeting.json', 'utf8'));
-    messages.channel = "C02JLJFPJ5S"
-    const today = getToday()
-    const countdown = 31 - today[3]
-    messages.blocks[4].elements[0].text = today[0] + "（2022年まであと" + countdown + "日）"
-
-    const headers = {
-        "content-type": "application/json",
-        "Authorization": 'Bearer ' + API_KEY
-    }
-
-    // API CALL
-    try { 
-        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
-        console.log(response.data); 
-    } catch (error) { 
-        console.log(error.response.body); 
-    } 
-}
 
 // API
 app.get('/api/v1/', (request, response) => {
@@ -184,7 +49,7 @@ app.post('/api/v1/endpoint', (request, response) => {
                 [value]: [respondent]
             }
             console.log("attendants: ", attendants)
-            updateAttendanceCheckPoll(timestamp, attendants)
+            functions.updateAttendanceCheckPoll(timestamp, attendants)
         }
         else if (typeof attendants[value] === "undefined") {
             attendance.update({
@@ -198,7 +63,7 @@ app.post('/api/v1/endpoint', (request, response) => {
             }
             attendance.update(attendants)
             attendants[value] = [respondent]
-            updateAttendanceCheckPoll(timestamp, attendants)
+            functions.updateAttendanceCheckPoll(timestamp, attendants)
         }
         else {
             for (const item in attendants) {
@@ -220,7 +85,7 @@ app.post('/api/v1/endpoint', (request, response) => {
                 }
             } 
             attendance.update(attendants);
-            updateAttendanceCheckPoll(timestamp, attendants)
+            functions.updateAttendanceCheckPoll(timestamp, attendants)
         }
     })
     
@@ -234,7 +99,7 @@ cron.schedule('0 10 * * *', () => {
     const dayOfWeek = getToday()[2]
     if (dayOfWeek != "土" || dayOfWeek != "日" ) {
         //土日以外実行されない
-        postAttendanceCheckPoll()
+        functions.postAttendanceCheckPoll()
     }
 });
 
@@ -242,7 +107,7 @@ cron.schedule('0 13 * * *', () => {
     const dayOfWeek = getToday()[2]
     if (dayOfWeek != "土" || dayOfWeek != "日" ) {
         //土日以外実行されない
-        postAttendanceCheckRemind()
+        functions.postAttendanceCheckRemind()
     }
 });
 
@@ -250,6 +115,6 @@ cron.schedule('15 9 * * *', () => {
     const dayOfWeek = getToday()[2]
     if (dayOfWeek != "土" || dayOfWeek != "日" ) {
         //土日以外実行されない
-        postCloudMeeting()
+        functions.postCloudMeeting()
     }
 });
---
+++ src/functions.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const weatherData = require('./weather.js')
+// Slack Configureation
+// const CHANNEL_ID = process.env.CHANNEL_ID_PROD; // kmp_kk_出社状況確認
+const CHANNEL_ID = process.env.CHANNEL_ID_TEST; // baymax-sandbox
+const API_KEY = process.env.API_KEY
+const API_ENDPOINT = "https://slack.com/api"
+
+exports.getToday = function getToday () {
+    // Date
+    const date  = new Date();
+    const year  = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day   = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const dayOfWeek = date.getDay()
+    const dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek]
+    const today = year + '年' + month + '月' + day + '日' + '(' + dayOfWeekStr + ')';
+    const todayString = year.toString() + month.toString() + day.toString()
+    return [today, todayString, dayOfWeekStr, day]
+}
+
+exports.postAttendanceCheckPoll = async function postAttendanceCheckPoll(){
+    // Weather
+    const weather = await weatherData()
+
+    // Edit messages
+    const messages = JSON.parse(fs.readFileSync('./src/message_template.json', 'utf8'));
+    messages.channel = CHANNEL_ID
+    messages.attachments[0].blocks[0].text.text = exports.getToday()[0] + "の出社状況"
+    messages.attachments[0].blocks[2].text.text =`*出社状況を教えてください。*<!channel>\n今日の大崎の天気: ${weather.description}\n:small_orange_diamond: 最高気温: *${weather.maxTemp}℃*\n:small_blue_diamond: 最低気温: *${weather.minTemp}℃*`
+    messages.attachments[0].blocks[2].accessory.image_url = weather.iconUrl
+    // Headers
+    const headers = {
+        "content-type": "application/json",
+        "Authorization": 'Bearer ' + API_KEY
+    }
+
+    
+    // API CALL
+    try { 
+        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
+        console.log(response.data); 
+    } catch (error) { 
+        console.log(error.response.body); 
+    } 
+}
+
+exports.updateAttendanceCheckPoll = async function updateAttendanceCheckPoll(timestamp, attendants){
+    messages.ts = timestamp
+    for (item in attendants) {
+        const text = attendants[item].join(',')
+        console.log(attendants[item].length)
+        const cnt = attendants[item].length
+        if (item == "home") {
+            messages.attachments[1].blocks[0].text.text = "*在宅*\n" + text
+            messages.attachments[1].blocks[1].elements[0].text = "合計" + cnt + "人"
+        }
+        else if (item == "osaki") {
+            messages.attachments[2].blocks[0].text.text = "*大崎*\n" + text
+            messages.attachments[2].blocks[1].elements[0].text = "合計" + cnt + "人"
+        }
+        else {
+            messages.attachments[3].blocks[0].text.text = "*その他*\n" + text
+            messages.attachments[3].blocks[1].elements[0].text = "合計" + cnt + "人"
+        }
+    }
+    // Headers
+    const headers = {
+        "content-type": "application/json",
+        "Authorization": 'Bearer ' + API_KEY
+    }
+    console.log(messages)
+    // API CALL
+    try { 
+        const response = await axios.post(API_ENDPOINT + "/chat.update", messages, { headers: headers })
+        console.log(response.data); 
+    } catch (error) { 
+        console.log(error.response.body); 
+    } 
+}
+
+exports.postAttendanceCheckRemind = async function postAttendanceCheckRemind(){
+    // Edit messages
+    const messages = JSON.parse(fs.readFileSync('./src/message_template.json', 'utf8'));
+    messages.channel = CHANNEL_ID
+    messages.attachments[0].blocks[0].text.text = exports.getToday()[0] + "の出社状況（リマインド）"
+    messages.attachments[0].blocks[2].text.text = ":bangbang:*未回答の方はご回答お願いいたします。*:bangbang:"
+    delete messages.attachments[0].blocks[2].accessory
+    messages.attachments.splice(1,messages.attachments.length - 1)
+    messages.attachments[0].blocks.splice(3, 1)
+
+    const headers = {
+        "content-type": "application/json",
+        "Authorization": 'Bearer ' + API_KEY
+    }
+
+    // API CALL
+    try { 
+        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
+        console.log(response.data); 
+    } catch (error) { 
+        console.log(error.response.body); 
+    } 
+}
+
+exports.postCloudMeeting = async function postCloudMeeting(){
+    // Edit messages
+    const messages = JSON.parse(fs.readFileSync('./src/message_template_cloud_meeting.json', 'utf8'));
+    messages.channel = "C02JLJFPJ5S"
+    // messages.channel = CHANNEL_ID
+    const today = exports.getToday()
+    const countdown = 31 - today[3]
+    messages.blocks[4].elements[0].text = ":mscalendar: " + today[0] + "（2022年まであと" + countdown + "日）"
+
+    const headers = {
+        "content-type": "application/json",
+        "Authorization": 'Bearer ' + API_KEY
+    }
+
+    // API CALL
+    try { 
+        const response = await axios.post(API_ENDPOINT + "/chat.postMessage", messages, { headers: headers })
+        console.log(response.data); 
+    } catch (error) { 
+        console.log(error.response.body); 
+    } 
+}
