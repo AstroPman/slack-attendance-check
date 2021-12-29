4fba2d152def20e601f429d8ab05770375af3e0d
KoppePan0709 <yuichi.masutani@ibm.com>
2021-12-29 18:53:13 +0900

    debug

--- src/modal.js
+++ src/modal.js
@@ -209,8 +209,9 @@ exports.removeInputForm = async function (requestJson) {
 
 exports.postPoll = async function (requestJson) {
     console.log(requestJson.view.state.values)
-    const title = requestJson.view.state.values[0]['plain_text_input-action'].value
-    const description = requestJson.view.state.values[1]['plain_text_input-action'].value
+    const keys = Object.keys(requestJson.view.state.values)
+    const title = requestJson.view.state.values[keys[0]]['plain_text_input-action'].value
+    const description = requestJson.view.state.values[keys[0]]['plain_text_input-action'].value
 
     const elements = requestJson.view.blocks[3].elements.slice(0, -1)
     const options = []
