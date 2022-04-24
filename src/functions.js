5ac92121aef07d871faabf29c7416822afc533e4
Yuichi Masutani <yuichi.masutani@kyndryl.com>
2022-04-24 11:43:57 +0900

    de bug

--- src/functions.js
+++ src/functions.js
@@ -12,7 +12,7 @@ const CHANNEL_ID = process.env.CHANNEL_ID_PROD; // kmp_kk_出社状況確認
 const API_KEY = process.env.API_KEY
 const API_ENDPOINT = "https://slack.com/api"
 
-console.log('Env: ', process.env)
+console.log('Env: ', process.env.TEST_JSON)
 
 
 
